3e7fa1a68000ccb9517e3ef7c2f1b06729035ed5
Akhil <a.akhilkumar74@gmail.com>
2024-09-01 14:44:11 +0530

    added import fixes

--- iris_classifier/train_pipeline.py
+++ iris_classifier/train_pipeline.py
@@ -1,5 +1,11 @@
 # loading libraries
 import sys
+from pathlib import Path
+
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
